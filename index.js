37e739a70615ed958fde3c4f8037b903ec5f4b79
ann nungari <annnjoroge98765@gmail.com>
2025-03-17 21:28:55 +0300

    arrays alteration

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+//const cats = ["Milo", "Otis", "Garfield"];
+let cats = ["Milo", "Otis", "Garfield"];
+//cats.push("Ralph");
+//cats.unshift("cat")
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+}
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield);
+}
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+}
+//const cats = ["Milo", "Otis", "Garfield"];
+function appendCat(Ralph) {
+return [...cats, Ralph];
+
+}
+copyOfCatsUsingSlice = cats.slice(Ralph);
+
+function prependCat(Bob) {
+    return [Bob, ...cats] ;
+    
+    }
+    copyOfCatsUsingSlice = cats.slice(Bob);
+
+    function removeFirstCat() {
+        return cats.slice(1);
+    }
+    function removeLastCat() {
+        return cats.slice(0,cats.length - 1);
+    }
+    
